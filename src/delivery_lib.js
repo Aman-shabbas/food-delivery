5ab04a4116439ae376df25a0e2895a8d40a682c9
Aman Shabbas <83913651+Aman-shabbas@users.noreply.github.com>
2025-01-31 16:08:36 +0530

    updated and cleaned

--- src/delivery_lib.js
+++ src/delivery_lib.js
@@ -1,47 +1,58 @@
-const getTimeTakenInSeconds = (startTime) => {
-  const totalTimeTaken = Math.floor((Date.now() - startTime) / 100);
-  const timeTakenInSeconds = (totalTimeTaken / 10).toFixed(2);
-  return timeTakenInSeconds;
+const secondsElapsedFrom = (referenceTime) => {
+  return (Math.floor(Date.now() / 10) - referenceTime * 100) / 100;
 };
 
-export const takeOrder = (startTime) => {
-  const orderDetails = { orderId: 123 };
-  const timeTakenForTakingOrder = getTimeTakenInSeconds(startTime);
+const printOrderDetails = (orderDetails, status) => {
+  const secondsTakenInString = secondsElapsedFrom(orderDetails.placedTime);
+  const detailsString = JSON.stringify(orderDetails);
   console.log(
-    `[${timeTakenForTakingOrder}s]Order recieved:${JSON.stringify(
-      orderDetails
-    )}`
+    `[${secondsTakenInString.toFixed(2)}s] ${status}:${detailsString}`
   );
-
-  prepareOrder(startTime, orderDetails);
 };
 
-const prepareOrder = (startTime, orderDetails) => {
-  const timeTakenForPreparingOrder = getTimeTakenInSeconds(startTime);
-  console.log(`[${timeTakenForPreparingOrder}s]Preparing order`);
+function printStatus(orderDetails) {
+  const timeTakenForPreparingOrder = secondsElapsedFrom(
+    orderDetails.placedTime
+  ).toFixed(2);
+  console.log(`[${timeTakenForPreparingOrder}s] Preparing order`);
+}
+
+export const takeOrder = () => {
+  const placedTime = Math.floor(Date.now() / 10) / 100;
+  const orderDetails = { orderId: 123, placedTime };
+  printOrderDetails(orderDetails, "Order recieved");
+
+  prepareOrder(orderDetails);
+};
+
+const prepareOrder = (orderDetails) => {
+  printStatus(orderDetails);
 
   setTimeout(() => {
     orderDetails.foodDetails = "Burger & Fries";
-    const timeTakenForCompletePreparation = getTimeTakenInSeconds(startTime);
-    console.log(
-      `[${timeTakenForCompletePreparation}s] Food is ready ${JSON.stringify(
-        orderDetails
-      )}`
-    );
 
-    packOrder(startTime, orderDetails);
+    printOrderDetails(orderDetails, "Food is ready..");
+
+    packOrder(orderDetails);
   }, 3 * 1000);
 };
 
-const packOrder = (startTime, orderDetails) => {
-  const packingStartTime = getTimeTakenInSeconds(startTime);
-  console.log(`[${packingStartTime}s] Packing food..`);
+const packOrder = (orderDetails) => {
+  printStatus(orderDetails);
 
   setTimeout(() => {
     orderDetails.packageDetails = "Packed in eco-friendly box";
-    const packingEndTime = getTimeTakenInSeconds(startTime);
-    console.log(
-      `[${packingEndTime}s] order packed.. ${JSON.stringify(orderDetails)}`
-    );
+    printOrderDetails(orderDetails, "Order packed");
+
+    deliver(orderDetails);
   }, 2 * 1000);
+};
+
+const deliver = (orderDetails) => {
+  printStatus(orderDetails);
+
+  setTimeout(() => {
+    orderDetails.deliveryDetails = "Delivered by John at 7:30 PM";
+    printOrderDetails(orderDetails, "Order delivered");
+  }, 5 * 1000);
 };
